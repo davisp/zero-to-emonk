3413765818456a615caf50b5a8f0f6cb4806bb84
Paul J. Davis <paul.joseph.davis@gmail.com>
2011-03-22 16:03:30 -0400

    Simplified code in first_nif.

--- apps/first_nif/c_src/first_nif.c
+++ apps/first_nif/c_src/first_nif.c
@@ -1,33 +1,15 @@
-
 #include "erl_nif.h"
 
-static int
-on_load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
-{
-    return 0;
-}
-
-static int
-on_reload(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
-{
-    return 0;
-}
-
-static int
-on_upgrade(ErlNifEnv* env, void** priv, void** old_priv, ERL_NIF_TERM info)
-{
-    return 0;
-}
-
 static ERL_NIF_TERM
-sayhi(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+say_hi(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    return enif_make_string(env, "Hello, world!", ERL_NIF_LATIN1);
+    const char* mesg = "Hello, World!";
+    return enif_make_string(env, mesg, ERL_NIF_LATIN1);
 }
 
 static ErlNifFunc nif_funcs[] =
 {
-    {"sayhi", 0, sayhi}
+    {"say_hi", 0, say_hi}
 };
 
-ERL_NIF_INIT(first_nif, nif_funcs, &on_load, &on_reload, &on_upgrade, NULL);
+ERL_NIF_INIT(first_nif, nif_funcs, NULL, NULL, NULL, NULL);
--- apps/first_nif/src/first_nif.erl
+++ apps/first_nif/src/first_nif.erl
@@ -1,30 +1,15 @@
 -module(first_nif).
--export([sayhi/0]).
-
--define(APPNAME, first_nif).
--define(LIBNAME, first_nif).
+-export([say_hi/0]).
 
 -on_load(init/0).
 
-
-sayhi() ->
+say_hi() ->
     not_loaded(?LINE).
 
-
 init() ->
-    SoName = case code:priv_dir(?APPNAME) of
-        {error, bad_name} ->
-            case filelib:is_dir(filename:join(["..", priv])) of
-                true ->
-                    filename:join(["..", priv, ?LIBNAME]);
-                _ ->
-                    filename:join([priv, ?LIBNAME])
-            end;
-        Dir ->
-            filename:join(Dir, ?LIBNAME)
-    end,
+    PrivDir = code:priv_dir(?MODULE),
+    SoName = filename:join(PrivDir, ?MODULE),
     erlang:load_nif(SoName, 0).
-
 
 not_loaded(Line) ->
     exit({not_loaded, [{module, ?MODULE}, {line, Line}]}).
